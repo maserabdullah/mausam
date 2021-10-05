1c3595ad6269203146fae5b1cb6d2311f06b05e1
Abdullah <maserabdullah@gmail.com>
2021-10-05 12:08:22 +0500

    Removed few console.log statements

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,11 +4,9 @@ const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWFzZXJhYmR1bGxhaCIsImEiOiJja2F5NGNiYWQwMDl5MnFwanIxZzZvam13In0.ygG-HEymtbGBtKBcAlTvKw&limit=1';
 
     request({url, json:true}, (error, {body}) => {
-        console.log(body);
         if(error) {
             callback('Unable to connect to internet', undefined);
         } else if(body.features.length === 0) {
-            // console.log(body);
             callback('Unable to find location. Please try another search', undefined);
         } else {
             callback(undefined, {
